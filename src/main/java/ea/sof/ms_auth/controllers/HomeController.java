2df4062c90c0213d630ed62d0c7dd9f2d833ceed
tnvle <tnle@mum.edu>
2019-11-17 10:39:01 -0600

    change home controller

--- src/main/java/ea/sof/ms_auth/controllers/HomeController.java
+++ src/main/java/ea/sof/ms_auth/controllers/HomeController.java
@@ -4,7 +4,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HomeController {
 
     @GetMapping("/")
