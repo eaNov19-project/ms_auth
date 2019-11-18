c2711a82229ba39dba5f0e4e6a6a2cdafa997210
rustem bayetov <rustem.bayetov@gmail.com>
2019-11-18 17:38:53 -0600

    home controller

--- src/main/java/ea/sof/ms_auth/controllers/HomeController.java
+++ src/main/java/ea/sof/ms_auth/controllers/HomeController.java
@@ -6,11 +6,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public ResponseEntity<?> index(){
-        return ResponseEntity.ok().build();
-    }
-}
+    public ResponseEntity<?> index() {
+        String host = "Unknown host";
+        try {
+            host = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>("Auth service. Host: " + host, HttpStatus.OK);
+    }}
